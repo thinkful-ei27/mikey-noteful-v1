9d4176556a87051304e577d710fa8b690ab09357
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-17 14:50:02 -0500

    listening on local host 8080, downloaded express, error message GET in DOM

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 'use strict';
 
-// Load array of notes
-const data = require('./db/notes');
-
 console.log('Hello Noteful!');
 
-// INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const data = require('./db/notes');
+const app = express();
+// ADD STATIC server here
+
+app.listen(8080, function() {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
