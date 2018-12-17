cca4a41e5cf835bacdff97ad9d72e45a69b0a782
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-17 15:33:11 -0500

    html in postman, but not the object??

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ console.log('Hello Noteful!');
 const express = require('express');
 
 const app = express();
-
+// Static server VV
 app.use(express.static('public'));
 
-// app.get('/api/notes', (req, res) => {
-//   res.json(data);
-// });
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
