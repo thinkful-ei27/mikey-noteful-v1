656d038ef1941d71589d1bcc79b39aba0a9fcd75
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-17 15:23:51 -0500

    forgot app.use line  explains why it wasnt working

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 'use strict';
 
+
+const data = require('./db/notes');
+
 console.log('Hello Noteful!');
 
 const express = require('express');
-const data = require('./db/notes');
-const app = express();
-// ADD STATIC server here
 
-app.listen(8080, function() {
+const app = express();
+
+app.use(express.static('public'));
+
+// app.get('/api/notes', (req, res) => {
+//   res.json(data);
+// });
+
+app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
 });
+
