9555a74ed34f98bd4108f48c2c801d8ace103326
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-17 15:57:01 -0500

    got the id request working, for loop did not work so i sused find

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ app.get('/api/notes', (req, res) => {
   res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res)=> {
+  const { id } = req.params;
+  
+  let requestedNote = data.find(item => item.id === Number(id));
+  // for (let i = 0; i < data.length; i++){
+  //   if(data[i].id === id ){
+  //     requestedNote = data[i];
+  //   }
+  
+  res.json(requestedNote);
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
