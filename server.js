ed8ef63d2cf78ef7f29802bdb2832dd2a3ad1eb7
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-19 14:34:48 -0500

    morgan log up and running, type = dev

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ console.log('Hello Noteful!');
 const express = require('express');
 
 const app = express();
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
-app.use(requestLogger);
 // Static server VVV, below logger to log .static get request
 app.use(express.static('public'));
 app.use(express.json());
