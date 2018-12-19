629d953d7f072e2837a4cacffb94ea4a31428be3
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-19 15:34:15 -0500

    POST working, goddamn post man dont forget content type

--- router/notes.router.js
+++ router/notes.router.js
@@ -57,6 +57,29 @@ router.put('/notes/:id', (req, res, next) => {
   });
 });
 
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  //  always validate requests
+  if(!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  notes.create(newItem, (err, item) => {
+    if(err){
+      return next(err);
+    }
+    if(item) {
+      res.location(`http://${req.headers.host}/api/notes/${item.id}`).status(201).json(item);
+    }
+    else {
+      next();
+    }
+  });
+});
+
 // app.get('/boom' , (req, res, next) => {
 //   throw new Error('Boom !!');
 // });
