ee15b7f2278888500bbe3baced14e2ac6def67ba
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-20 15:25:27 -0500

    put not update yet

--- router/notes.router.js
+++ router/notes.router.js
@@ -56,17 +56,29 @@ router.put('/notes/:id', (req, res, next) => {
     }
   });
 
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
+  notes.update(id, updateObj)
+    .then( item =>{
+      if(item){
+        res.json(item);
+      } else { 
+        next();
+      }
+    })
+    .catch(err => next(err));
 });
+
+
+
+// (err, item) => {
+//   if (err) {
+//     return next(err);
+//   }
+//   if (item) {
+//     res.json(item);
+//   } else {
+//     next();
+//   }
+// });
 
 router.post('/notes', (req, res, next) => {
   const { title, content } = req.body;
