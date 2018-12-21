9b15c898a0524c46f87612d76ba8662fac34dfbc
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-21 15:36:41 -0500

    first tests running, dont forget module.export

--- server.js
+++ server.js
@@ -37,10 +37,12 @@ app.use(function (err, req, res, next) {
 });
 
 
+if(require.main === module){
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
 
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
-
+module.exports = app;
---
+++ test/server.test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const app = require('../server');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+
+describe('Reality Check', function () {
+  it( 'true should be true', function () {
+    expect(true).to.be.true;
+  });
+  it ('2 + 2 should equal 4' , function () { 
+    expect(2 + 2).to.equal(4);
+  });
+});
+describe( 'Express static', function () {
+
+  it('GET request "/" should return the index page', function () {
+    return chai.request(app)
+      .get('/')
+      .then( function (res) {
+        expect(res).to.exist;
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+      });
+  });
+
+});
+
+describe('404 handler', function () {
+
+  it('should respond with a 404 when given a bad path', function () {
+    return chai.request(app)
+      .get('/DOES/NOT/EXIST')
+      .then( function(res){
+        expect(res).to.have.status(404);
+      });
+  });
+});
